863719a7724a58076572dacf939ef7d9ef2caf2a
tatsuki <c0a2411782@edu.teu.ac.jp>
2025-12-02 16:50:34 +0900

    コメントアウト追加

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -11,6 +11,11 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外を判定し，真理値タプルを返す関数
+    引数：こうかとんや爆弾，ビームなどのRect
+    戻り値：横方向，縦方向のはみ出し判定結果（画面内：True／画面外：False）
+    """
     yoko, tate = True, True
     if obj_rct.left < 0 or WIDTH < obj_rct.right:
         yoko = False
@@ -38,7 +43,7 @@ class Score:
 
 class Bird:
     """
-    こうかとんの移動及び方向画像に関するクラス
+    ゲームキャラクター（こうかとんに関するクラス）
     """
     delta = {
         pg.K_UP: (0, -5),
@@ -47,28 +52,42 @@ class Bird:
         pg.K_RIGHT: (+5, 0),
     }
     img0 = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
-    img = pg.transform.flip(img0, True, False)
-    imgs = {
-        (+5, 0): img,
-        (+5, -5): pg.transform.rotozoom(img, 45, 0.9),
-        (0, -5): pg.transform.rotozoom(img, 90, 0.9),
-        (-5, -5): pg.transform.rotozoom(img0, -45, 0.9),
-        (-5, 0): img0,
-        (-5, +5): pg.transform.rotozoom(img0, 45, 0.9),
-        (0, +5): pg.transform.rotozoom(img, -90, 0.9),
-        (+5, +5): pg.transform.rotozoom(img, -45, 0.9),
+    img = pg.transform.flip(img0, True, False)# デフォルトのこうかとん（右向き）
+    imgs = { # 0度から反時計回りに定義
+        (+5, 0): img, #右
+        (+5, -5): pg.transform.rotozoom(img, 45, 0.9), #右上
+        (0, -5): pg.transform.rotozoom(img, 90, 0.9), #上
+        (-5, -5): pg.transform.rotozoom(img0, -45, 0.9), #左上
+        (-5, 0): img0, #左
+        (-5, +5): pg.transform.rotozoom(img0, 45, 0.9), #左下
+        (0, +5): pg.transform.rotozoom(img, -90, 0.9), #下
+        (+5, +5): pg.transform.rotozoom(img, -45, 0.9), #右上
     }
 
     def __init__(self, xy: tuple[int, int]):
+        """
+        こうかとん画像Surfaceを生成する
+        引数 xy：こうかとん画像の初期位置座標タプル
+        """
         self.img = __class__.imgs[(+5, 0)]
         self.rct = self.img.get_rect()
         self.rct.center = xy
 
     def change_img(self, num: int, screen: pg.Surface):
+        """
+        こうかとん画像を切り替え，画面に転送する
+        引数1 num：こうかとん画像ファイル名の番号
+        引数2 screen：画面Surface
+        """
         self.img = pg.transform.rotozoom(pg.image.load(f"fig/{num}.png"), 0, 0.9)
         screen.blit(self.img, self.rct)
 
     def update(self, key_lst: list[bool], screen: pg.Surface):
+        """
+        押下キーに応じてこうかとんを移動させる
+        引数1 key_lst：押下キーの真理値リスト
+        引数2 screen：画面Surface
+        """
         sum_mv = [0, 0]
         for k, mv in __class__.delta.items():
             if key_lst[k]:
@@ -112,6 +131,11 @@ class Bomb:
     爆弾に関するクラス
     """
     def __init__(self, color: tuple[int, int, int], rad: int):
+        """
+        引数に基づき爆弾円Surfaceを生成する
+        引数1 color：爆弾円の色タプル
+        引数2 rad：爆弾円の半径
+        """
         self.img = pg.Surface((2*rad, 2*rad))
         pg.draw.circle(self.img, color, (rad, rad), rad)
         self.img.set_colorkey((0, 0, 0))
@@ -122,6 +146,10 @@ class Bomb:
         self.vx, self.vy = +5, +5
 
     def update(self, screen: pg.Surface):
+        """
+        爆弾を速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
         yoko, tate = check_bound(self.rct)
         if not yoko:
             self.vx *= -1
@@ -155,7 +183,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
 
-        # --- こうかとん vs 爆弾 ---
+        # --- こうかとんand爆弾 ---
         for b, bomb in enumerate(bombs):
             if bird.rct.colliderect(bomb.rct):
                 bird.change_img(8, screen)
